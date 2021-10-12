c6cab5a28dd59936f8fab18371237a19c054b554
Courtney Cash <cashcourtney97@gmail.com>
2021-10-12 11:41:29 -0400

    commit tree f35ebe0c662e845893dca53144bcc2f8dc20f116

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, length: { minimum: 100}
+    validates :content, inclusion: { in: ['Fiction', 'Non-Fiction'] }
 end
